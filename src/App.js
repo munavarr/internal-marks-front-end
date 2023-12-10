ab227ca2e073f128d345686b2a3c2a97b7b7be4f
munavarr <mnvrlogs@gmail.com>
2023-12-10 11:19:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Addmembers from './pages/admin/Addmembers';
 import AddTeacher from './pages/admin/AddTeacher';
-import AddStudent from './pages/admin/AddStudent';
-import AddSubject from './pages/admin/AddSubject';
+// import AddStudent from './pages/admin/AddStudent';
+// import AddSubject from './pages/admin/AddSubject';
 import Adminlogin from './pages/admin/Adminlogin'
-import Teacherslogin from './pages/teachers/Teacherslogin';
-import Teacher from './pages/teachers/Teacher';
+// import Teacherslogin from './pages/teachers/Teacherslogin';
+// import Teacher from './pages/teachers/Teacher';
 
 
 function App() {
@@ -18,9 +18,9 @@ function App() {
           <Route path="/" element={<Adminlogin />} />
           <Route path="/AddTeacher" element={<AddTeacher />} />
           <Route path="/Addmembers" element={<Addmembers />} />
-          <Route path="/AddStudent" element={<AddStudent />} />
-          <Route path="/AddSubject" element={<AddSubject />} />
-          <Route path="/Teacher" element={<Teacher />} />
+          {/* <Route path="/AddStudent" element={<AddStudent />} /> */}
+          {/* <Route path="/AddSubject" element={<AddSubject />} /> */}
+          {/* <Route path="/Teacher" element={<Teacher />} /> */}
         </Routes>
       </Router>
 
